050e0549e80aa14aa4da2e34e8a0eb58b2739905
shpatmustafa <shpatxmustafa@gmail.com>
2022-04-09 13:25:28 +0200

    Updated Navigation Style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,32 +6,32 @@ const Nav = () => {
     <div className="nav">
       <ul className="ul1">
         <li className="li1">
-          <NavLink className="link" to="/home">
-            HOME
+          <NavLink className="link" to="/">
+            <p>HOME</p>
             <i className="bx bx-home home-icon  first-icon"></i>
           </NavLink>
         </li>
         <li className="li1">
           <Link className="link" to="/about" activeClassName="active-nav">
-            ABOUT
+            <p>ABOUT</p>
             <i className="bx bx-user home-icon"></i>
           </Link>
         </li>
         <li className="li1">
           <Link className="link" to="/portfolio" activeClassName="active-nav">
-            PORTFOLIO
+            <p>PORTFOLIO</p>
             <i className="bx bx-briefcase home-icon"></i>
           </Link>
         </li>
         <li className="li1">
           <Link className="link" to="/contact" activeClassName="active-nav">
-            CONTACT
+            <p>CONTACT</p>
             <i className="bx bx-envelope-open home-icon"></i>
           </Link>
         </li>
         <li className="li1">
           <Link className="link" to="/">
-            BLOG
+            <p>BLOG</p>
             <i className="bx bx-message-rounded home-icon"></i>
           </Link>
         </li>
