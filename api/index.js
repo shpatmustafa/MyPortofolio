1aa42dc48497bdc8c857180b3aeafb204fe8d80b
shpatmustafa <shpatxmustafa@gmail.com>
2022-04-10 15:47:20 +0200

    Finished Front-end

--- api/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const port = 3000;
-
-require("./config/database").connect();
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
